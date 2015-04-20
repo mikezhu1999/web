f966cd78f5bf268283163f42944128db2e4259eb
Michael Zhu <mike_zhu_2002@hotmail.com>
2015-04-20 15:57:03 -0700

    basic Java

---
+++ java/basic.java
@@ -0,0 +1,3 @@
+// for long, Java has been widely used by web project and is very robust
+// it's easier to manage than C++ and has huge libraries to get things done at backend
+// it's challenged by new scripting language like Python, NodeJS and GO 
